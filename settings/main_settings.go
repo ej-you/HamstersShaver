88abfbbc893ad9c12895ea5e92e9b7d9e4aa17b3
Danil-114195722 <ejyou.user@gmail.com>
2024-06-26 01:18:32 +0300

    Added main and tg_bot settings

---
+++ settings/main_settings.go
@@ -0,0 +1,19 @@
+package settings
+
+import (
+	"log"
+	"os"
+)
+
+
+// логеры
+var InfoLog *log.Logger = log.New(os.Stdout, "[INFO]\t", log.Ldate|log.Ltime)
+var ErrorLog *log.Logger = log.New(os.Stderr, "[ERROR]\t", log.Ldate|log.Ltime|log.Lshortfile)
+var fatalLog *log.Logger = log.New(os.Stderr, "[FATAL]\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+// функция для обработки критических ошибок
+func DieIf(err error) {
+	if err != nil {
+		fatalLog.Panic(err)
+	}
+}
