6a54ba31ca475243453cf8776b4a74282c8f04c9
Danil-114195722 <ejyou.user@gmail.com>
2024-08-13 23:45:33 +0300

    Added TonPoolAddr to constants

--- settings/constants/constants.go
+++ settings/constants/constants.go
@@ -1,8 +1,12 @@
 package constants
 
 
-const StonfiRouterAddr = "EQB3ncyBUTjZUA5EnFKR5_EnOMI9V1tTEAAPaiU71gc4TiUt"
+const (
+	StonfiRouterAddr = "EQB3ncyBUTjZUA5EnFKR5_EnOMI9V1tTEAAPaiU71gc4TiUt"
 
-const ProxyTonMasterAddr = "EQCM3B12QK1e4yZSf8GtBRT0aLMNyEsBc_DhVfRRtOEffLez"
-const MemeMasterAddr = "EQAWpz2_G0NKxlG2VvgFbgZGPt8Y1qe0cGj-4Yw5BfmYR5iF"
-const NotMasterAddr = "EQAvlWFDxGF2lXm67y4yzC17wYKD9A0guwPkMs1gOsM__NOT"
+	ProxyTonMasterAddr = "EQCM3B12QK1e4yZSf8GtBRT0aLMNyEsBc_DhVfRRtOEffLez"
+	TonPoolAddr = "EQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAM9c"
+
+	MemeMasterAddr = "EQAWpz2_G0NKxlG2VvgFbgZGPt8Y1qe0cGj-4Yw5BfmYR5iF"
+	NotMasterAddr = "EQAvlWFDxGF2lXm67y4yzC17wYKD9A0guwPkMs1gOsM__NOT"
+)
