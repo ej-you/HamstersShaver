f772d4b7c0d1fe39b78a7f1e66e66f8509309c2b
Danil-114195722 <ejyou.user@gmail.com>
2024-12-22 20:09:41 +0300

    Some fix in main error handler

--- rest_api/core/error_handler/error_handler.go
+++ rest_api/core/error_handler/error_handler.go
@@ -13,15 +13,10 @@ import (
 // настройка обработчика ошибок
 func CustomErrorHandler(echoApp *echo.Echo) {
 	echoApp.HTTPErrorHandler = func(err error, ctx echo.Context) {
-		// является ли ошибка структурой *echo.HTTPError
+		// если ошибка не является структурой *echo.HTTPError
 		httpError := new(echo.HTTPError)
 		if !errors.As(err, &httpError) {
 			httpError = echo.NewHTTPError(500, map[string]string{"unknown": err.Error()})
-		}
-		// если пришла *echo.HTTPError ошибка со строкой в качестве httpError.Message
-		stringErrorMessage, ok := (httpError.Message).(string)
-		if ok {
-			httpError.Message = map[string]string{"unknown": stringErrorMessage}
 		}
 
 		errMessage := map[string]interface{}{
@@ -35,7 +30,7 @@ func CustomErrorHandler(echoApp *echo.Echo) {
 		// отправка ответа
 		respErr := ctx.JSON(httpError.Code, errMessage)
 		if respErr != nil {
-			settings.ErrorLog.Println("Failed to send error response:", respErr)
+			settings.ErrorLog.Println("failed to send error response:", respErr)
 		}
 	}
 }
