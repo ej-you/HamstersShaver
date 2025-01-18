ee5102d089c5af8f8b07f80edfd5b98238b1195c
Danil-114195722 <ejyou.user@gmail.com>
2025-01-18 20:17:29 +0300

    Tried some pytonapi funcs

--- rest_api_py/pytonapi_test.py
+++ rest_api_py/pytonapi_test.py
@@ -6,6 +6,9 @@ from pytonapi.schema.events import TransactionEventData
 # Enter your API key
 API_KEY = os.getenv("TON_API_KEY")
 
+SUCCESS_TRANS_HASH = "99821a8101a7d25e76811e01e97d606d5caa5f62419867245b8fd1f5f362590b"
+FAILED_TRANS_HASH = "9225f9db67e9857e177c1e3570de26c157e94feed7d989f63244972879833a0a"
+
 # List of TON blockchain accounts to monitor
 ACCOUNTS = [
     "UQDLjMTSj58tHMc9oIeo00dbV6smPp1plQw5vUSZADeUInM9",
@@ -32,9 +35,10 @@ async def handler(event: TransactionEventData, tonapi: AsyncTonapi) -> None:
     :param event: The SSEvent object containing transaction details.
     :param tonapi: An instance of AsyncTonapi for interacting with TON API.
     """
-    # trace = await tonapi.traces.get_trace(event.tx_hash)
-    #
-    print(event)
+    print("event:", event)
+
+    trace = await tonapi.traces.get_trace(event.tx_hash)
+    print("trace:", trace)
 
 
 async def main() -> None:
@@ -42,9 +46,30 @@ async def main() -> None:
     print("tonapi:", tonapi)
 
     # Subscribe to transaction events for the specified accounts
-    await tonapi.sse.subscribe_to_transactions(
-        handler, accounts=ACCOUNTS, args=(tonapi,)
-    )
+    # await tonapi.sse.subscribe_to_transactions(
+    #     handler, accounts=ACCOUNTS, args=(tonapi,)
+    # )
+
+    # trans_hash = "837edbb2f337f27f1e25c7d87299d468303656e716f051e8b4ad4301d5e0c8b3"
+    trans_trace = await tonapi.traces.get_trace(FAILED_TRANS_HASH)
+    # print("one trace:", trace)
+
+    def trace_unwrap(trace):
+        print("trace:", trace.transaction)
+        print()
+        if trace.children:
+            trace_unwrap(trace.children[0])
+
+    trace_unwrap(trans_trace)
+
+
+async def main2() -> None:
+    tonapi = AsyncTonapi(api_key=API_KEY)
+    print("tonapi:", tonapi)
+
+    trans_data = await tonapi.blockchain.get_transaction_data(transaction_id=FAILED_TRANS_HASH)
+    print("trans_data.success:", trans_data.success)
+    print("trans_data:", trans_data)
 
 
 if __name__ == "__main__":
