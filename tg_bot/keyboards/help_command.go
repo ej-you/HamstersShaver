d7b591338304d30f3cbadce71c58ba509f1489f4
Ej_you <113062021+ej-you@users.noreply.github.com>
2025-01-20 20:50:01 +0300

    Delete tg_bot directory

--- tg_bot/keyboards/help_command.go
+++
@@ -1,9 +0,0 @@
-package keyboards
-
-import (
-	telebot "gopkg.in/telebot.v3"
-)
-
-
-var InlineKeyboardHelp = &telebot.ReplyMarkup{}
-var BtnHideHelp = InlineKeyboardHelp.Data("скрыть справку", "hide_help")
--- tg_bot/keyboards/start_command.go
+++
@@ -1,9 +0,0 @@
-package keyboards
-
-import (
-	telebot "gopkg.in/telebot.v3"
-)
-
-
-var InlineKeyboardToHome = &telebot.ReplyMarkup{}
-var BtnToHome = InlineKeyboardToHome.Data("главное меню", "to_home")
